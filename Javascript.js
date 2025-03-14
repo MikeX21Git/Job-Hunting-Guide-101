c5284a1f9c22e31913435682f52b33d2521a4694
MikeX21Git <lawdmikethegamer@gmail.com>
2025-03-14 05:31:21 -0700

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to Job Hunting Guide!");
+
+    // Example: Adding an alert when clicking on a button (if you add one)
+    // document.getElementById("someButton").addEventListener("click", function() {
+    //     alert("Button clicked!");
+    // });
+});
